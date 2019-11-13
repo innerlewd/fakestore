936768e381bbbb078582454b82d96d34154eed25
unknown <innerlewd@gmail.com>
2019-11-12 20:27:18 -0500

    added schema and seed for database

---
+++ seeds.sql
@@ -0,0 +1,15 @@
+USE fakestore
+INSERT INTO department VALUES (department_name, over_head_costs) VALUES ('Electronics', 10000);
+INSERT INTO department VALUES (department_name, over_head_costs) VALUES ('Clothing', 60000);
+
+INSERT INTO product VALUES (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Gameboy', 1, 50, 2000, 0);
+INSERT INTO product VALUES (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Gameboy DS', 1, 75, 2000, 0);
+INSERT INTO product VALUES (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Xbox', 1, 50, 2000, 0);
+INSERT INTO product VALUES (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Playstation', 1, 75, 2000, 0);
+INSERT INTO product VALUES (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Switch', 1, 50, 2000, 0);
+
+INSERT INTO product VALUES (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Hats', 2, 75, 2000, 0);
+INSERT INTO product VALUES (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Shirts', 2, 50, 2000, 0);
+INSERT INTO product VALUES (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Shoes', 2, 75, 2000, 0);
+INSERT INTO product VALUES (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Socks', 2, 50, 2000, 0);
+INSERT INTO product VALUES (product_name, department_id, price, stock_quantity, product_sales) VALUES ('Gloves', 2, 75, 2000, 0);
